804f7b03fb2d105b700c4d939a133b4dbb8257cf
ktscates <cates2506@gmail.com>
2024-09-30 01:19:51 +0200

    unit testing

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyASU22jgliUoAzQviEr4LVryTPCj8oSfcc',
+    authDomain: 'entertainment-app-ee1db.firebaseapp.com',
+    projectId: 'entertainment-app-ee1db',
+    storageBucket: 'entertainment-app-ee1db.appspot.com',
+    messagingSenderId: '1079228727161',
+    appId: '1:1079228727161:web:ce764d3608aa80f57db9a8',
+  },
+
+  apiKey: 'c40cb525ac8706f5611fc6cfe4c1419b',
+  apiToken:
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDBjYjUyNWFjODcwNmY1NjExZmM2Y2ZlNGMxNDE5YiIsIm5iZiI6MTcyNzQyNDg0NC43NzcxNTUsInN1YiI6IjYwY2YxMGMxOWMyNGZjMDA1OGIyZjgyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bB0NNR_rpNO0yXd4JxWcSQ80jcESlL8mMiy_I6S_S_c',
+}
